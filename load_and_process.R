98441cb505ef3506c6210f818e81105b4ca99758
Sergey Samsonau <s.samsonau@gmail.com>
2015-12-15 22:26:39 -0500

    auto unpack corrected. Predictors and output modified. Imputation of data added. Basic analysis through Confusion Matrix added

--- load_and_process.R
+++ load_and_process.R
@@ -1,73 +1,81 @@
 #load the data table faster
 library(data.table)
-if(!exists("data/train.csv")) unzip("data/train.csv.zip", exdir = "data")
-if(!exists("data/test.csv")) unzip("data/test.csv.zip", exdir = "data")
+if(!file.exists("./data/train.csv")) unzip("data/train.csv.zip", exdir = "data")
+if(!file.exists("data/test.csv")) unzip("data/test.csv.zip", exdir = "data")
 
 DT <- fread("data/train.csv")
 DT2 <- fread("data/test.csv")
 
+## use only part of data, while testing algorithm
 DT <- DT[1:1000, ]
 
-#Split training dataset into training/validation & Cross Validation
+## Split training dataset into training/validation & Cross Validation
 library(caret)
 set.seed(100)
 inTraining <- createDataPartition(DT$Ret_121, p = .75, list = FALSE)
 inTraining
 training <- DT[inTraining[, 1], ]
 validation  <- DT[-inTraining[, 1], ]
+
+## defining vector of predictors and outcome variabl
+predictors <- names(DT2)
+predictors <- predictors[predictors != "Id"]
+
+outcome_var <- names(DT)
+outcome_var <- outcome_var[! (outcome_var %in% names(DT2) )]
+
+## Imputing missing values
+preProcObj <- preProcess(method="bagImpute", training[, .SD, .SDcols=predictors])
+#preProcObj <- preProcess(method="knnImpute", training[, .SD, .SDcols=predictors])
+
+training[, eval(predictors):= predict(preProcObj, 
+                                      training[, .SD, .SDcols=predictors])]
+
+validation[, eval(predictors):= predict(preProcObj, 
+                                        validation[, .SD, .SDcols=predictors])]
+
+DT2[, eval(predictors):= predict(preProcObj, 
+                                        DT2[, .SD, .SDcols=predictors])]
+
+
+## Training model and making predictions
 fitControl <- trainControl(
   method = "oob",
-  #number = 2, 
   verboseIter = T
-  ## repeated ten times
-  #repeats = 10
 )
 
-training <- na.omit(training)
+## Loop - making model and doing predictions for evey outcome variable independently 
 
-pr <- c(grep("Ret_[1-9]", names(DT), value=T)[120:179],grep("Ret_[Plus]", names(DT), value=T)[1:2])
+#data table to collect resutls
+resDT <- data.table(Id=DT2[, Id])
+#file for print intermideate results
+if( file.exists("CM.txt") ) file.remove("CM.txt")
 
-DT2 = na.omit(DT2)
-resDT <- data.table(data.frame(remove_it=DT2$Ret_2))
-
-#Loop 
-for(n in pr){
+for(n in outcome_var[1:2]){ #use [1:2 if only two out needed - for testing]
   #Random Force model
   cat("Doing model and predition for: ", n)
-  fit <- train(training[, .SD, .SDcols=grep("Ret_[1-9]", names(training), value=T)[1:119]], training[, get(n)],
+  fit <- train(training[, .SD, .SDcols=predictors], training[, get(n)],
                method = "rf",
                trControl = fitControl,
-               ## This last option is actually one
-               ## for gbm() that passes through
                verbose = FALSE)
   fit
-  plot(fit)
-  validation = na.omit(validation)
+  #plot(fit)
   
-  #Validation of the data
-  out <- predict.train(fit, newdata=
-                         validation[, .SD, .SDcols=grep("Ret_[1-9]", names(validation), value=T)[1:119]])
+  #predition on validation set
+  out <- predict(fit, newdata = validation[, .SD, .SDcols=predictors])
   
-  plot(out)
-  plot(validation$Ret_121)
-  
-  #Table the sign of corresponding elements
-  s_out <- sign(out) == sign(validation$Ret_121)
-  table(s_out)
-  
-  #Correlation 
-  cor(out, validation$Ret_121)
+  #basic analysis of accuracy
+  out_sign <- as.logical( sign(out)+1 )
+  real_sign <- validation[, as.logical( sign(get(n)) +1 )]
+  CM <- confusionMatrix(out_sign, real_sign)
+  capture.output(CM, file = "CM.txt", append = T)
   
   #Predict the outcome in testing dataset
-  predict.train(fit, newdata=
-                  DT2[, .SD, .SDcols=grep("Ret_[1-9]", names(DT2), value=T)[1:119]])
-  
-  res_temp <- predict.train(fit, newdata=
-                              DT2[, .SD, .SDcols=grep("Ret_[1-9]", names(DT2), value=T)[1:119]])
-  
+  res_temp <- predict(fit, newdata = DT2[, .SD, .SDcols=predictors])
+  #saving resultss of a current loop
   resDT[, eval(n):=res_temp]
 }
 
-resDT[, remove_it:=NULL]
-
-write.table(resDT,file="out.csv", quote = F)
+#writing format should be adjusted
+#short resDT format - to long, before writing
+write.csv(resDT,file="out.csv", quote = F, row.names = F)
